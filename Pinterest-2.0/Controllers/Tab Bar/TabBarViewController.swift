11669580be178732a8d7ecba294b2e6b58eb068c
RenaruX <renarux.92@gmail.com>
2019-07-08 13:44:03 -0500

    style update and general fixes

---
+++ Pinterest-2.0/Controllers/Tab Bar/TabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarViewController.swift
+//  Pinterest-2.0
+//
+//  Created by Renato Berrocal on 7/8/19.
+//  Copyright © 2019 Renato Berrocal. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
